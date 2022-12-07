4ff4424dbbd8b065ec870f26426acf9a8b0e539a
DanStelmakh <104099587+DanStelmakh@users.noreply.github.com>
2022-12-07 10:33:17 +0000

    Deploying to gh-pages from @ DanStelmakh/IceCream@dd14aaf72d22db69d70a22f77ddda63f6c340462 🚀

---
+++ index.0a9bf095.js
@@ -0,0 +1,2 @@
+new Swiper(".mySwiper",{pagination:{el:".swiper-pagination",clickable:!0}});
+//# sourceMappingURL=index.0a9bf095.js.map
---
+++ index.83ac0818.js
@@ -0,0 +1,2 @@
+new Swiper(".mySwiper",{pagination:{el:".swiper-pagination",clickable:!0}});
+//# sourceMappingURL=index.83ac0818.js.map
